4e73d335ee72d81b30e48b43d7cfcb1fa871a810
Wojciech Michalowski <wojciech.michalowski1@wp.pl>
2023-12-14 01:22:14 +0100

    added implementation for service and fix errors

---
+++ src/main/java/com/example/todo/service/TaskServiceImpl.java
@@ -0,0 +1,57 @@
+package com.example.todo.service;
+
+import com.example.todo.dto.TaskDto;
+import com.example.todo.model.Task;
+import com.example.todo.repository.TaskRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Task getTaskById(Long taskId) {
+        return taskRepository.findById(taskId).orElse(null);
+    }
+
+    @Override
+    public Task createTask(TaskDto taskDto) {
+        Task newTask = new Task();
+        newTask.setTitle(taskDto.getTitle());
+        newTask.setCompleted(false);
+        return taskRepository.save(newTask);
+    }
+
+    @Override
+    public Task updateTask(Long taskId, TaskDto updatedTask) {
+        Optional<Task> existingTaskOptional = taskRepository.findById(taskId);
+        if (existingTaskOptional.isPresent()) {
+            Task existingTask = existingTaskOptional.get();
+            existingTask.setTitle(updatedTask.getTitle());
+            existingTask.setCompleted(updatedTask.isCompleted());
+            return taskRepository.save(existingTask);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteTask(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+}
